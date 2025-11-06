2cd6c8c6933ce09df02bddbadb6d9fb359ddf491
SDL-HQ <info+gh@manaakihealth.co.nz>
2025-11-07 12:11:15 +1300

    Update __init__.py

--- sir_firewall/__init__.py
+++ sir_firewall/__init__.py
@@ -1,1 +1,4 @@
-from .sir_firewall import validate_sir
+from .validator import validate_sir, WHITELIST, MAX_INPUT_BYTES
+
+__all__ = ["validate_sir", "WHITELIST", "MAX_INPUT_BYTES"]
+__version__ = "1.0.2"
